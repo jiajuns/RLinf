7febc854dcd6156ece4583030844c0ac318e5984
jiajuns757-design <jiajuns757@gmail.com>
2026-09-19 22:24:09 +0800

    fix: force replace split GPU placement map

--- examples/embodiment/slurm_pirl_flow_sde_smoke.sh
+++ examples/embodiment/slurm_pirl_flow_sde_smoke.sh
@@ -83,7 +83,7 @@ fi
 placement_overrides=()
 if [[ -n "$PIRL_ENV_GPU" ]]; then
   placement_overrides+=(
-    "cluster.component_placement={actor: 0, rollout: 0, env: ${PIRL_ENV_GPU}}"
+    "++cluster.component_placement={actor: 0, rollout: 0, env: ${PIRL_ENV_GPU}}"
   )
 fi
 
