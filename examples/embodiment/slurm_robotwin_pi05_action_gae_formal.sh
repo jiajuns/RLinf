32d56e7220d4ec6abba0b415b7b6df19580b2f12
jiajuns757-design <jiajuns757@gmail.com>
2026-09-20 18:57:12 +0800

    fix: append Flow-SDE joint logprob flag for RoboTwin

--- examples/embodiment/slurm_robotwin_pi05_action_gae_formal.sh
+++ examples/embodiment/slurm_robotwin_pi05_action_gae_formal.sh
@@ -74,7 +74,7 @@ exec python examples/embodiment/train_embodied_agent.py \
   algorithm.adv_type=gae \
   algorithm.loss_type=actor_critic \
   actor.model.openpi.noise_method=flow_sde \
-  actor.model.openpi.joint_logprob=false \
+  +actor.model.openpi.joint_logprob=false \
   actor.model.openpi.value_after_vlm=true \
   actor.global_batch_size="${ROBOTWIN_GLOBAL_BATCH}" \
   actor.micro_batch_size="${ROBOTWIN_MICRO_BATCH}"
